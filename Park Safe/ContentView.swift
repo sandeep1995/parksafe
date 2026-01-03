9c06fbcfecd96b68846ad7423cfe84abdb336663
Sandeep Acharya <i.am.sandeep.acharya@gmail.com>
2026-01-03 21:03:32 +0530

    Initial Commit

---
+++ Park Safe/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Park Safe
+//
+//  Created by Sandeep Acharya on 03/01/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Park Safe/Park_SafeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Park_SafeApp.swift
+//  Park Safe
+//
+//  Created by Sandeep Acharya on 03/01/26.
+//
+
+import SwiftUI
+
+@main
+struct Park_SafeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
